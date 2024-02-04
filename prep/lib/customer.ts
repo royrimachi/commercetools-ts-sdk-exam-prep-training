b0dc107a2a9f47efb0d38c69160b6e4eaf383de5
Roy Joseph Rimachi Costillas <royexrc@gmail.com>
2024-02-04 18:42:09 -0500

    feat: create customer function implemented

--- prep/lib/customer.ts
+++ prep/lib/customer.ts
@@ -1,14 +1,25 @@
 import { apiRoot } from "./client";
-import { ClientResponse, Customer, CustomerDraft, CustomerSignInResult } from "@commercetools/platform-sdk";
+import {
+  ClientResponse,
+  Customer,
+  CustomerDraft,
+  CustomerSignInResult,
+} from "@commercetools/platform-sdk";
 
-export const getCustomerById = (ID: string): Promise<ClientResponse<Customer>> =>{
-    throw new Error("Function not implemented")
-}
+export const getCustomerById = (
+  ID: string
+): Promise<ClientResponse<Customer>> => {
+  throw new Error("Function not implemented");
+};
 
-export const getCustomerByKey = (key: string): Promise<ClientResponse<Customer>> =>{
-    throw new Error("Function not implemented")
-}
+export const getCustomerByKey = (
+  key: string
+): Promise<ClientResponse<Customer>> => {
+  throw new Error("Function not implemented");
+};
 
-export const createCustomer = (customerDraft: CustomerDraft): Promise<ClientResponse<CustomerSignInResult>> =>{
-    throw new Error("Function not implemented")
-}
+export const createCustomer = (
+  customerDraft: CustomerDraft
+): Promise<ClientResponse<CustomerSignInResult>> => {
+  return apiRoot.customers().post({ body: customerDraft }).execute();
+};
